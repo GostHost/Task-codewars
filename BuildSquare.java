8c840ef204a1bcd80da7630a8088fc48a4695862
GostHost <calmghost@yandex.ru>
2022-12-20 14:36:05 +0400

    add comments

---
+++ BuildSquare.java
@@ -0,0 +1,13 @@
+package Task;
+
+public class BuildSquare {
+    public static void main(String[] args) {
+        int x = 5; //anything number
+        for (int i = 0; i < x; i ++){
+            System.out.println();
+            for (int j = 0; j < x; j ++){
+                System.out.print("+");
+            }
+        }
+    }
+}
